0da2958f8b22deef47fafb18ac9f971924524c9b
Liuzi <769120167@qq.com>
2015-12-14 20:38:59 +0800

    bee

---
+++ src/Bee.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Bee extends FlyingObject{
+	private int abs_x=1;
+	private int xSpeed = abs_x;     //x坐标走步步数
+	private int ySpeed = 2;     //y坐标走步步数
+	private int awardType;      //奖励类型
+	
+	public Bee(){
+		width = 25;
+		height = 25;
+		Random rand = new Random();
+		x = rand.nextInt(380 - this.width);
+	    y = -this.height; 
+		awardType = rand.nextInt(2);//随机生成奖励类型(0:life++,1:fire++)	
+	}
+	
+	public int getType(){
+		return awardType;
+	}
+    public void step(){   	
+    		if(x >= 380 - this.width){
+        		xSpeed = -abs_x;
+    		
+    		}else if(x <= 0){
+				xSpeed = abs_x;
+			}
+    			
+    	x += xSpeed;
+    	y += ySpeed;	
+	}
+	
+	@Override
+	public boolean outOfBounds() {
+		return this.y > 560;  
+	}
+	
+
+}
